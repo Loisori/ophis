ac22c11498714e511ff0badead5bb0c6ace54a09
loihuynh <loisori1001@gmail.com>
2025-11-21 15:52:38 +0700

    fix structure #4

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,33 +1,14 @@
-import Hero from "@/components/section/Hero";
+"use server";
+
 import Header from "@/components/section/Header";
-import HeroVideo from "@/components/section/HeroVideo";
 import Footer from "@/components/section/Footer";
-import Timeline from "@/components/section/Timeline";
-import AutoCarousel from "@/components/section/AutoCarousel";
-import Projects from "@/components/section/Projects";
-import Testimonials from "@/components/section/Testimonials";
-import Reasons from "@/components/section/Reasons";
-import Team from "@/components/section/Team";
-import Pricing from "@/components/section/Pricing";
-import Faqs from "@/components/section/Faqs";
-import Services from "@/components/section/Services";
+
 
 export default function AdminHome() {
   return (
     <main>
       <Header />
-      <Hero />
-      <AutoCarousel />
-      <HeroVideo />
-      <Timeline />
-      {/* <Projects />
-      <Testimonials />
-      <Services />
-      <Reasons />
-      <Team />
-      <Pricing />
-      <Faqs />
-      <Footer /> */}
+      <Footer />
     </main>
   );
 }
